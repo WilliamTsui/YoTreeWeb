7efd9f3c18fe2d8a8535fb3bdf3248b9470f28f0
WilliamTsui <danevc@163.com>
2024-10-31 22:47:30 +0800

    20241031 1.0 Create Project

---
+++ main/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -------------------------------
+
+# @Product Name：PyCharm
+# @Python version：3.12.0
+
+# -------------------------------
+
+# @Script Name：__init__.py.py
+# @Create time：2024/10/31 下午10:26
+# @Email：danevc@163.com
+
+# -------------------------------
---
+++ main/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# -------------------------------
+
+# @Product Name：PyCharm
+# @Python version：3.12.0
+
+# -------------------------------
+
+# @Script Name：main.py
+# @Create time：2024/10/31 下午10:27
+# @Email：danevc@163.com
+
+# -------------------------------
+import django as dj
+
